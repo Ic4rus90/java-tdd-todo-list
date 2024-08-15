d21e204c29142f9488f4452c763cd8d85dd024be
Thomas Wiik <thomas.wiik@no.experis.com>
2024-08-15 14:31:42 +0200

    Added implementation of sorting, both ascending and descending.

--- src/main/java/com/booleanuk/core/TodoList.java
+++ src/main/java/com/booleanuk/core/TodoList.java
@@ -1,33 +1,35 @@
 package com.booleanuk.core;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class TodoList {
-    private ArrayList<Task> taskList = new ArrayList<>();
+    public ArrayList<Task> taskList = new ArrayList<>();
     private Integer taskFilter = 0;
-    private Integer sorting = 0;
 
     public boolean addTask(Task task) {
         /*
         try - catch blocks are hardly necessary, but I wanted to experiment with different solutions.
          */
         try {
-            taskList.add(task);
+            this.taskList.add(task);
             return true;
         } catch (Exception e) {
             return false;
         }
     }
     public int listSize(){
-        return taskList.size();
+        return this.taskList.size();
     }
 
 
     public void viewTasks(){
         switch(this.taskFilter){
             case 0: {
-                for (Task t : taskList) {
+                for (Task t : this.taskList) {
                     String taskName = t.getTaskName();
                     System.out.println(taskName);
                 }
@@ -35,7 +37,7 @@ public class TodoList {
             }
 
             case 1: {
-                for (Task t : taskList) {
+                for (Task t : this.taskList) {
                     if (!t.getIsCompleted()) {
                         String taskName = t.getTaskName();
                         System.out.println(taskName);
@@ -45,7 +47,7 @@ public class TodoList {
             }
 
             case 2: {
-                for (Task t : taskList) {
+                for (Task t : this.taskList) {
                     if (t.getIsCompleted()) {
                         String taskName = t.getTaskName();
                         System.out.println(taskName);
@@ -63,7 +65,7 @@ public class TodoList {
     }
 
     public String searchTask(String task){
-        for (Task t : taskList) {
+        for (Task t : this.taskList) {
             if (Objects.equals(t.getTaskName(), task)){
                 return "Task was found in your list";
             }
@@ -73,18 +75,30 @@ public class TodoList {
 
 
     public String removeTask(String task){
-        for (Task t : taskList){
+        for (Task t : this.taskList){
             if (Objects.equals(t.getTaskName(), task)){
-                taskList.remove(t);
+                this.taskList.remove(t);
                 return "Task removed";
             }
         }
         return "Task does not exist";
     }
 
-    /*
-    public void setSorting(Integer sortingChoice){
 
+    public void setSorting(Integer sortingChoice){
+        // 0 for ascending, 1 for descending.
+        ArrayList<Task> sorted = new ArrayList<>();
+
+        if (sortingChoice == 0){
+            this.taskList = this.taskList.stream()
+                    .sorted(Comparator.comparing(Task::getTaskName))
+                    .collect(Collectors.toCollection(ArrayList::new));
+        }
+
+        else if (sortingChoice == 1){
+            this.taskList = this.taskList.stream()
+                    .sorted((task_2, task_1) -> task_1.getTaskName().compareTo(task_2.getTaskName()))
+                    .collect(Collectors.toCollection(ArrayList::new));
+        }
     }
- */
 }
